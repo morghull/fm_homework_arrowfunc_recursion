31b480f81165122ed8f3b28c5f2f47da08ae3271
morghull <morghull@gmail.com>
2021-10-12 08:48:25 +0300

    add task4 - power with arrow function

--- js/task.js
+++ js/task.js
@@ -34,3 +34,14 @@ const taskArr = [
 const maxValue = Math.max(...taskArr);
 const minValue = Math.min(...taskArr);
 
+console.log('Task array:', taskArr);
+console.log('Max value:', maxValue);
+console.log('Min value:', minValue);
+
+//task4
+const powerWithArrow = (...args) => {
+  return args.reduce((prev, curr) => prev * curr);
+};
+
+console.log('1*2*3*4*5=', powerWithArrow(1, 2, 3, 4, 5));
+console.log('2*3=', powerWithArrow(2, 3));
