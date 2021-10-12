6ff37db222db9a15c6286257ea8f1d589056bc6f
morghull <morghull@gmail.com>
2021-10-12 08:47:24 +0300

    add task2 - pari brackets

--- js/task.js
+++ js/task.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//task1
 const power = (num, pow) => {
   if (pow < 0) return 1 / power(num, -pow);
   if (pow === 1) {
@@ -8,7 +9,20 @@ const power = (num, pow) => {
   return num * power(num, pow - 1);
 };
 
-console.log(power(2,2));
-console.log(power(2,3));
-console.log(power(2,-1));
-console.log(power(2,-2));
+console.log(power(2, 2));
+console.log(power(2, 3));
+console.log(power(2, -1));
+console.log(power(2, -2));
+
+//task2
+const pairBrackets = (num) => {
+  if (num < 1) return '()';
+  return '(' + pairBrackets(num - 1) + ')';
+};
+
+console.log(pairBrackets(-1));
+console.log(pairBrackets(0));
+console.log(pairBrackets(1));
+console.log(pairBrackets(2));
+console.log(pairBrackets(3));
+console.log(pairBrackets(4));
